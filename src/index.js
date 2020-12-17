0bf05a88db2022a45bc8e841bd5a496686a37300
mydearxym <mydearxym@qq.com>
2020-12-17 12:18:51 +0800

    chore: basic gallery card ui

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import { make } from '@groupher/editor-utils'
+import { make } from "@groupher/editor-utils";
 /**
  * Build styles
  */
 import css from "./index.css";
-import tippy, { hideAll } from 'tippy.js'
-import 'tippy.js/dist/tippy.css';
-import 'tippy.js/themes/light.css';
+import tippy, { hideAll } from "tippy.js";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/themes/light.css";
 
-import Icons from './iconIndex'
+import Icons from "./iconIndex";
 
+const peopleImgSrc =
+  "https://rmt.dogedoge.com/fetch/~/source/unsplash/photo-1557555187-23d685287bc3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80";
 
 /**
  * People Block for the Editor.js.
@@ -51,7 +53,7 @@ export default class People {
   static get toolbox() {
     return {
       icon: `<svg width="19" t="1595923402082" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="15249"><path d="M384 469.333333a170.666667 170.666667 0 1 0-170.666667-170.666666 170.666667 170.666667 0 0 0 170.666667 170.666666zM725.333333 554.666667a128 128 0 1 0-128-128 128 128 0 0 0 128 128zM896 853.333333a42.666667 42.666667 0 0 0 42.666667-42.666666 213.333333 213.333333 0 0 0-343.893334-168.533334A298.666667 298.666667 0 0 0 85.333333 853.333333a42.666667 42.666667 0 0 0 42.666667 42.666667h512a42.666667 42.666667 0 0 0 42.666667-42.666667" p-id="15250"></path></svg>`,
-      title: "人物/团队"
+      title: "人物/团队",
     };
   }
 
@@ -68,28 +70,24 @@ export default class People {
 
     this.CSS = {
       block: this.api.styles.block,
-      wrapper: "cdx-column",
-      innerWrapper: "cdx-column-inner-wrapper",
-      column: "cdx-column-part",
-      columnHead: "cdx-column-head",
-      columnSpot: "cdx-column-spot",
-      columnTitle: "cdx-column-title",
-      columnBody: "cdx-column-body",
-      // dots
-      selector: "cdx-color-selector",
-      selectDot: "cdx-color-selector-dot",
-      activeDot: "cdx-color-selector-dot--active",
+      wrapper: "cdx-people",
+      galleryWrapper: "cdx-people-gallery-wrapper",
+      galleryAvatar: "cdx-people-gallery-avatar",
+      galleryBox: "cdx-people-gallery-box",
+      galleryBoxTitle: "cdx-people-gallery-box-title",
+      galleryBoxBio: "cdx-people-gallery-box-bio",
+      galleryBoxDesc: "cdx-people-gallery-box-desc",
     };
 
     this.data = {
       title: data.title || "",
-      content: data.content || ""
+      content: data.content || "",
     };
 
     this.TitleInput = null;
     this.CollapseContent = null;
 
-    this.element = this.drawView();
+    this.element = null;
     this.data = data;
   }
 
@@ -100,106 +98,59 @@ export default class People {
    */
   drawView() {
     const Wrapper = make("DIV", [this.CSS.block, this.CSS.wrapper], {});
-    const InnerWrapper = make("DIV", [this.CSS.innerWrapper]);
+    const GalleryWrapper = make("DIV", [this.CSS.galleryWrapper]);
 
-    this.ColumnLeft = this.drawColumn('left')
-    this.ColumnRight = this.drawColumn('right')
+    const avatarEl = this.buildGalleryAvatar();
+    const galleryCardEl = this.buildGalleryCard();
 
-    InnerWrapper.appendChild(this.ColumnLeft);
-    InnerWrapper.appendChild(this.ColumnRight);
+    GalleryWrapper.appendChild(avatarEl);
+    GalleryWrapper.appendChild(galleryCardEl);
 
-    Wrapper.appendChild(InnerWrapper);
+    Wrapper.appendChild(GalleryWrapper);
 
     return Wrapper;
   }
-
-  reDrawView() {
-    this.element = this.drawView()
-  }
-
   /**
-   * Create left or right column
+   * create a gallery card
    * @return {HTMLElement}
    * @private
    */
-  drawColumn(part) {
-    const Wrapper = make("DIV", [this.CSS.column]);
-    const Head = make("DIV", [this.CSS.columnHead]);
-
-    const ActiveIcon = Icons[0]
-    const Spot = make("DIV", [this.CSS.columnSpot], {
-      innerHTML: ActiveIcon.icon
+  buildGalleryAvatar() {
+    const avatarEl = make("img", this.CSS.galleryAvatar, {
+      src: peopleImgSrc,
     });
-    const Title = make("DIV", [this.CSS.columnTitle], {
+
+    return avatarEl;
+  }
+
+  /**
+   * create a gallery card
+   * @return {HTMLElement}
+   * @private
+   */
+  buildGalleryCard() {
+    const boxEl = make("DIV", this.CSS.galleryBox);
+
+    const titleEl = make("DIV", this.CSS.galleryBoxTitle, {
+      innerHTML: "我是山三",
       contentEditable: true,
-      placeholder: "输入标题 ..."
     });
 
-    const Body = make("DIV", [this.CSS.columnBody], {
-      placeholder: "输入内容 ...",
-      contentEditable: true
+    const bioEl = make("DIV", this.CSS.galleryBoxBio, {
+      innerHTML: "UI/UX designer",
+      contentEditable: true,
+    });
+    const descEl = make("DIV", this.CSS.galleryBoxDesc, {
+      innerHTML:
+        "特别是太空军事化竞赛中争取一个有利位置是至关重要的，但由于资金不足，这些饼有多少可以最终拿出成果，以当下的投入和进度而言，难言乐观",
+      contentEditable: true,
     });
 
-    Head.appendChild(Spot);
-    Head.appendChild(Title);
+    boxEl.appendChild(titleEl);
+    boxEl.appendChild(bioEl);
+    boxEl.appendChild(descEl);
 
-    Wrapper.appendChild(Head);
-    Wrapper.appendChild(Body);
-
-    this.api.listeners.on(Spot, 'click', () => {
-      tippy(Spot, this.drawSpotSelector(part, ActiveIcon))
-    }, false);
-
-    return Wrapper;
-  }
-
-  /**
-   * spot icon selector panel
-   * @param {string} part - 'left' or 'right'
-   * @param {object} activeIcon - current used icon in column
-   * @param {string} activeIcon.name - icon name
-   * @param {string} activeIcon.icon - icon svg content
-   * @return {HTMLElement}
-   * @private
-   */
-  drawSpotSelector(part, activeIcon) {
-    const Wrapper = make('div', [this.CSS.selector])
-
-    Icons.forEach(iconItem => {
-      const Dot = make('div', [this.CSS.selectDot], {
-        innerHTML: iconItem.icon
-      })
-
-      Wrapper.appendChild(Dot)
-
-      if (activeIcon.name === iconItem.name) {
-        Dot.classList.add(this.CSS.activeDot)
-      }
-
-      this.api.listeners.on(Dot, 'click', () => {
-        Wrapper.childNodes.forEach(item => {
-          item.classList.remove(this.CSS.activeDot)
-        })
-
-        const Column = part === 'left' ? this.ColumnLeft : this.ColumnRight
-
-        // console.log("ColumnLeft: ", this.ColumnRight)
-        const ColumnIcon = Column.getElementsByClassName(this.CSS.columnSpot)
-        ColumnIcon[0].innerHTML = Dot.innerHTML
-
-        Dot.classList.add(this.CSS.activeDot)
-      }, false);
-    })
-
-    return {
-      content: Wrapper,
-      theme: 'light',
-      // delay: 200,
-      trigger: "click",
-      placement: 'bottom',
-      // allowing you to hover over and click inside them.
-      interactive: true,
-    }
+    return boxEl;
   }
 
   /**
@@ -208,6 +159,7 @@ export default class People {
    * @public
    */
   render() {
+    this.element = this.drawView();
     return this.element;
   }
 
