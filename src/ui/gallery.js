8e68bec6b3f197aa0a0aa574206a971fe72a6e0c
mydearxym <mydearxym@qq.com>
2020-12-18 13:20:34 +0800

    refactor: preview ux/ui

--- src/ui/gallery.js
+++ src/ui/gallery.js
@@ -5,10 +5,34 @@ import css from "../styles/gallery.css";
 const peopleImgSrc =
   "https://rmt.dogedoge.com/fetch/~/source/unsplash/photo-1557555187-23d685287bc3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80";
 
+const peopleImgSrc2 =
+  "https://rmt.dogedoge.com/fetch/~/source/unsplash/photo-1484399172022-72a90b12e3c1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80";
+
 export default class Ui {
   constructor({ api, config }) {
     this.api = api;
     this.config = config;
+
+    this.data = {
+      type: "gallery", // or list
+      items: [
+        {
+          active: true,
+          avatar: peopleImgSrc,
+          title: "mydearxym",
+          bio: "life is great",
+          desc:
+            "特别是太空军事化竞赛中争取一个有利位置是至关重要的，但由于资金不足，这些饼有多少可以最终拿出成果，以当下的投入和进度而言，难言乐观",
+        },
+        {
+          avatar: peopleImgSrc2,
+          title: "simon2",
+          bio: "life is fucked",
+          desc:
+            "特别是太空军事化竞赛中争取一个有利位置是至关重要的，但由于资金不足，这些饼有多少可以最终拿出成果，以当下的投入和进度而言，难言乐观",
+        },
+      ],
+    };
   }
 
   /**
@@ -20,6 +44,10 @@ export default class Ui {
       galleryWrapper: "cdx-people-gallery-wrapper",
       galleryCard: "cdx-people-gallery-card",
       galleryPreviewerWrapper: "cdx-people-gallery-previewer-wrapper",
+      galleryPreviewerItem: "cdx-people-gallery-previewer-item",
+      galleryPreviewerItemActive: "cdx-people-gallery-previewer-item-active",
+      galleryPreviewerAvatar: "cdx-people-gallery-previewer-avatar",
+      galleryPreviewerTitle: "cdx-people-gallery-previewer-title",
       galleryAvatar: "cdx-people-gallery-avatar",
       galleryBox: "cdx-people-gallery-box",
       galleryBoxTitle: "cdx-people-gallery-box-title",
@@ -37,9 +65,9 @@ export default class Ui {
     const Wrapper = make("DIV", [this.CSS.galleryWrapper]);
     const CardEl = make("DIV", this.CSS.galleryCard);
 
-    const PreviewerEl = this.drawPreviewer();
-    const AvatarEl = this.drawAvatar();
-    const BoxEl = this.drawBox();
+    const PreviewerEl = this._drawPreviewer();
+    const AvatarEl = this._drawAvatar();
+    const BoxEl = this._drawBox();
 
     CardEl.appendChild(AvatarEl);
     CardEl.appendChild(BoxEl);
@@ -55,8 +83,39 @@ export default class Ui {
    * @return {HTMLElement}
    * @private
    */
-  drawPreviewer() {
-    const Wrapper = make("img", this.CSS.galleryPreviewerWrapper);
+  _drawPreviewer() {
+    const Wrapper = make("DIV", this.CSS.galleryPreviewerWrapper);
+
+    this.data.items.map((people) => {
+      const AvatarEl = this._drawPreviewAvatar(people);
+      Wrapper.appendChild(AvatarEl);
+    });
+
+    return Wrapper;
+  }
+
+  /**
+   * draw preview avatar
+   * @return {HTMLElement}
+   * @private
+   */
+  _drawPreviewAvatar(people) {
+    const wrapperCSS = people.active
+      ? [this.CSS.galleryPreviewerItem, this.CSS.galleryPreviewerItemActive]
+      : this.CSS.galleryPreviewerItem;
+
+    const Wrapper = make("div", wrapperCSS);
+
+    const AvatarEl = make("img", this.CSS.galleryPreviewerAvatar, {
+      src: people.avatar,
+    });
+
+    const TitleEl = make("div", this.CSS.galleryPreviewerTitle, {
+      innerHTML: people.title,
+    });
+
+    Wrapper.appendChild(AvatarEl);
+    Wrapper.appendChild(TitleEl);
 
     return Wrapper;
   }
@@ -66,7 +125,7 @@ export default class Ui {
    * @return {HTMLElement}
    * @private
    */
-  drawAvatar() {
+  _drawAvatar() {
     const AvatarEl = make("img", this.CSS.galleryAvatar, {
       src: peopleImgSrc,
     });
@@ -79,7 +138,7 @@ export default class Ui {
    * @return {HTMLElement}
    * @private
    */
-  drawBox() {
+  _drawBox() {
     const BoxEl = make("DIV", this.CSS.galleryBox);
     const TitleEl = make("INPUT", this.CSS.galleryBoxTitle, {
       contentEditable: true,
@@ -92,9 +151,10 @@ export default class Ui {
       placeholder: "简短描述",
     });
     const DescEl = make("DIV", this.CSS.galleryBoxDesc, {
-      innerHTML:
-        "特别是太空军事化竞赛中争取一个有利位置是至关重要的，但由于资金不足，这些饼有多少可以最终拿出成果，以当下的投入和进度而言，难言乐观",
+      innerHTML: "",
+      // "特别是太空军事化竞赛中争取一个有利位置是至关重要的，但由于资金不足，这些饼有多少可以最终拿出成果，以当下的投入和进度而言，难言乐观",
       contentEditable: true,
+      placeholder: "详细介绍",
     });
 
     BoxEl.appendChild(TitleEl);
